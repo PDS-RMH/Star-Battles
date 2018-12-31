7d8902a34936c8d38935483b0b33f7d6eb53ea95
PDS-RMH <46204620+PDS-RMH@users.noreply.github.com>
2018-12-30 23:49:08 -0700

    Added debug text to UI

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
     [SerializeField] float xRotationSpeed = 1.0f;
+    public Text xAcceleration;
+    public Text yAcceleration;
+    public Text zAcceleration;
+    public Text position;
+    public GameObject go;
 
-	// Update is called once per frame
-	void Update () {
 
-        Debug.Log(Input.acceleration.x);
-        Debug.Log(Input.acceleration.y);
-        Debug.Log(Input.acceleration.z);
-
-        //transform.Rotate(Input.acceleration.x, Input.acceleration.y, Input.acceleration.z);
+    private void Update()
+    {
+        ShowUIText();
     }
+
+    void ShowUIText()
+    {
+        xAcceleration.text = Input.acceleration.x.ToString();
+        position.text = "X Position" + this.transform.position.x.ToString();
+        
+    }
+
 }
