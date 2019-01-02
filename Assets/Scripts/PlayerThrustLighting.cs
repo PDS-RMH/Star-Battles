6259ee2d3528adec0f38857ea98af54d7bc94998
PDS-RMH <46204620+PDS-RMH@users.noreply.github.com>
2019-01-01 21:18:53 -0700

    Added thrust lighting

---
+++ Assets/Scripts/PlayerThrustLighting.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerThrustLighting : MonoBehaviour {
+
+    public Slider thrustSlider;
+    public float lightIntensityFactor = 0.2f;
+    public Light lt;
+
+	// Use this for initialization
+	void Start () {
+        thrustSlider.onValueChanged.AddListener(delegate { ValueChangeCheck(); });
+    }
+
+    public void ValueChangeCheck()
+    {
+        lt.intensity = thrustSlider.value * lightIntensityFactor;
+    }
+
+}
