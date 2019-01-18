4efc8228ea4ba21db8a334018e73883c6cbe2fbf
PDS-RMH <46204620+PDS-RMH@users.noreply.github.com>
2019-01-17 21:52:09 -0700

    AsteroidField First Commit

---
+++ Assets/Scripts/AsteroidFieldGenerator.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidFieldGenerator : MonoBehaviour
+{
+    // Width and height of the texture in pixels.
+    public int pixWidth;
+    public int pixHeight;
+
+    // The origin of the sampled area in the plane.
+    public float xOrg;
+    public float yOrg;
+
+    // The number of cycles of the basic noise pattern that are repeated
+    // over the width and height of the texture.
+    public float scale = 1.0F;
+
+    public float pixelValue = 0f;
+
+    private Texture2D noiseTex;
+    private Color[] pix;
+    private Renderer rend;
+
+    private Vector3[] asteroidArray;
+    private GameObject go;
+    private int asteroidSpreadXY = 5;
+    private float asteroidSpreadZ = 200f;
+
+
+    void Awake()
+    {
+        asteroidArray = new Vector3[255];
+        go = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        go.AddComponent<MeshRenderer>();
+    }
+    
+    
+
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+
+        // Set up the texture and a Color array to hold pixels during processing.
+        noiseTex = new Texture2D(pixWidth, pixHeight);
+        pix = new Color[noiseTex.width * noiseTex.height];
+        rend.material.mainTexture = noiseTex;
+
+        CalcNoise();
+        GenerateAsteroidArray();
+    }
+
+    void CalcNoise()
+    {
+        // For each pixel in the texture...
+        float y = 0.0F;
+
+        while (y < noiseTex.height)
+        {
+            float x = 0.0F;
+            while (x < noiseTex.width)
+            {
+                float xCoord = xOrg + x / noiseTex.width * scale;
+                float yCoord = yOrg + y / noiseTex.height * scale;
+                float sample = Mathf.PerlinNoise(xCoord, yCoord);
+                pix[(int)y * noiseTex.width + (int)x] = new Color(sample, sample, sample);
+                x++;
+            }
+            y++;
+        }
+
+        // Copy the pixel data to the texture and load it into the GPU.
+        noiseTex.SetPixels(pix);
+        noiseTex.Apply();
+    }
+
+    void GenerateAsteroidArray()
+    {
+
+        int y = 0;
+        while (y < pixHeight)
+        {
+            int x = 0;
+            while (x < pixWidth)
+            {
+                int i = 0;
+                if (noiseTex.GetPixel(x, y).grayscale > 0.5f)
+                {
+                    asteroidArray[i] = new Vector3(x * asteroidSpreadXY, y * asteroidSpreadXY, (noiseTex.GetPixel(x, y).grayscale * asteroidSpreadZ) - 50f);
+                    Instantiate(go, asteroidArray[i], new Quaternion(0f, 0f, 0f, 1f));
+                    x++;
+                    i++;
+                }
+             x++;
+            }
+            y++;
+        }
+
+    }
+
+
+    void Update()
+    {
+
+//        Debug.Log(noiseTex.GetPixel(20,21).grayscale);
+
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,18 +12,19 @@ public class PlayerMovement : MonoBehaviour {
     public Text yAcceleration;
     public Text zAcceleration;
     public Text position;
+    public Text velocityText;
     public GameObject go;
     public Rigidbody rb;
     public float attitudeSensitivity = 1f;
     public float thrust = 1f;
     public Button resetRotations;
     public Slider thrustSlider;
-    public Button leftButton;
-    public Button rightButton;
+    [SerializeField] float speedLimit = 100f;
+    public float playerVelocity;
 
     // Variables to read and store the instantaneous rotation when button is pressed
     private float xAccelerationDelta;
-    private float yAccelerationDelta;
+ //   private float yAccelerationDelta;
     private float zAccelerationDelta;
 
 
@@ -31,27 +32,46 @@ public class PlayerMovement : MonoBehaviour {
     {
         rb.GetComponent<Rigidbody>();
         resetRotations.onClick.AddListener(ResetRotationsFunction);
-//        leftButton.onClick.AddListener(rotateLeftFunction);
-//        rightButton.onClick.AddListener(rotateRightFunction);
         xAccelerationDelta = -Input.acceleration.x;
-        yAccelerationDelta = -Input.acceleration.y;
+ //       yAccelerationDelta = -Input.acceleration.y;
         zAccelerationDelta = -Input.acceleration.z;
-
     }
 
-    public void Update()
+
+    public void FixedUpdate()
     {
         // Verify system that game is running on
 #if UNITY_ANDROID
         AndroidPlayerControls();
 #endif
         PCPlayerControls();
-        Debug.Log(xAccelerationDelta);
+
+        SpeedLimit();
+
     }
 
-    private void FixedUpdate()
+
+    public void Update()
     {
         ShowUIText();
+    }
+
+
+    // Calculates and displays the velocity of the player, and limits the maximum velocity
+    public void SpeedLimit()
+    {
+        playerVelocity = Mathf.Round(rb.velocity.magnitude * 10f) / 10f;
+//        Debug.Log(playerVelocity);
+
+        if(playerVelocity > speedLimit)
+        {
+            rb.velocity = rb.velocity.normalized * speedLimit;
+            thrust = 0f;
+        }
+        else if(playerVelocity < speedLimit)
+        {
+            thrust = 1f;
+        }
     }
 
 
@@ -59,22 +79,11 @@ public class PlayerMovement : MonoBehaviour {
     public void ResetRotationsFunction()
     {
         xAccelerationDelta = -Input.acceleration.x;
-        yAccelerationDelta = -Input.acceleration.y;
+//        yAccelerationDelta = -Input.acceleration.y;
         zAccelerationDelta = -Input.acceleration.z;
 
     }
 
-    /*
-    public void rotateLeftFunction()
-    {
-        go.transform.Rotate(Vector3.left, attitudeSensitivity, Space.Self);
-    }
-
-    public void rotateRightFunction()
-    {
-        go.transform.Rotate(-Vector3.left, attitudeSensitivity, Space.Self);
-    }
-    */
 
     void ShowUIText()
     {
@@ -86,24 +95,27 @@ public class PlayerMovement : MonoBehaviour {
 
         //Attitude control
         zAcceleration.text = "Z Acceleration: " + Input.acceleration.z.ToString();
+
+        //Positional Text
         position.text = "X Position: " + this.transform.position.x.ToString();
-        
+
+        //Speed Text
+        velocityText.text = "Speed: " + playerVelocity.ToString();
+
     }
+
 
     public void AndroidPlayerControls()
     {
+        //Rotate player ship based on accelerometer inputs
         go.transform.Rotate(Vector3.forward, (Input.acceleration.z + zAccelerationDelta) * attitudeSensitivity, Space.Self);
         go.transform.Rotate(Vector3.up, (Input.acceleration.x + xAccelerationDelta) * attitudeSensitivity, Space.Self);
 
+        //Reverse Thrust for Slider
         rb.AddRelativeForce(0, thrustSlider.value * -thrust, 0, ForceMode.Force);
-        
-        /*
-        if (Input.GetButton(leftButton)) 
-        {
-            rb.AddRelativeForce(0, 0, thrust, ForceMode.Force);
-        }
-        */
+
     }
+
 
     public void PCPlayerControls()
         {
