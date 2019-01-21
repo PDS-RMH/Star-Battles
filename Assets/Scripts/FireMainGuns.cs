1c7a3e21ec419897203a12fa7bc7822bfe671038
PDS-RMH <46204620+PDS-RMH@users.noreply.github.com>
2019-01-20 23:15:06 -0700

    Fire UI Button Added

---
+++ Assets/Scripts/FireMainGuns.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FireMainGuns : MonoBehaviour {
+
+    public Button fireButton;
+
+    public GameObject go;
+    public Rigidbody rb;
+    public Transform tr;
+    public float laserSpeed = 100f;
+
+
+    // Use this for initialization
+    void Start () {
+        fireButton.onClick.AddListener(FireFunction);
+        rb = go.GetComponent<Rigidbody>();
+        tr = go.GetComponent<Transform>();
+    }
+	
+    public void FireFunction()
+    {
+        Rigidbody clone;
+        clone = Instantiate(rb, transform.position, transform.rotation);
+
+        // Give the cloned object an initial velocity along the current
+        // object's Z axis
+        clone.velocity = transform.TransformDirection(Vector3.right * laserSpeed);
+    }
+
+}
